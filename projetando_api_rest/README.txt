9c6f0151fafdf55e9dfef80d063928fe0dcdad56
Daniells1 <96155029+Daniells1@users.noreply.github.com>
2022-04-29 01:07:27 -0300

    Add files via upload

---
+++ projetando_api_rest/README.txt
@@ -0,0 +1,3 @@
+
+
+Este projeto  emprega Java para criar uma API rest.Fazendo então um conexão com o Amazon RDS.
---
+++ projetando_api_rest/src/main/java/com/dioclass/rdswithapirest/MyfirstApi/PersonController.java
@@ -0,0 +1,34 @@
+package com.dioclass.rdswithapirest.MyfirstApi;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+public class PersonController {
+
+    private final PersonRepository repositoryPerson;
+
+    public PersonController(PersonRepository repository) {
+        this.repositoryPerson = repository;
+    }
+
+    @GetMapping("/")
+    public String helloWorld(){
+        return "Oi esse é seu método de controle.";
+    }
+
+    @GetMapping("/persons")
+    public List<Person> personFindAll(){
+        return repositoryPerson.findAll();
+    }
+
+    @GetMapping("/persons/{id}")
+    public Optional personFindById(@PathVariable Long id){
+            return repositoryPerson.findById(id);
+    }
+
+}
